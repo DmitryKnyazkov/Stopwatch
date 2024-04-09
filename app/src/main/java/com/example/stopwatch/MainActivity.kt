5692394c30400e50f2359b854d336edf2f2d77bd
DmitryKnyazkov <kde.2015@yandex.ru>
2024-04-09 23:40:34 +0300

    Initial commit

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -0,0 +1,239 @@
+package com.example.stopwatch
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.example.stopwatch.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val viewModel1: ViewModelSW1 by viewModels()
+        val viewModel2: ViewModelSW2 by viewModels()
+        val viewModel34: ViewModelSW3 by viewModels()
+        val viewModel5: ViewModelSW5 by viewModels()
+
+
+
+        binding.btn12.setOnClickListener {
+            if (binding.btn12.text == "СТАРТ") {
+            viewModel1.startCounter()
+        } else {
+            viewModel1.pauseCounter()
+        }
+
+        }
+        lifecycleScope.launch {
+
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+
+                launch {
+                    viewModel1.getvalueFlow().collect {
+                        binding.tx13.text = it.toString()
+                    }
+                }
+
+                launch {
+                    viewModel1.getConditionFlow().collect {
+                        if (it == true) {
+                            binding.btn12.text = "СТОП"
+                        }
+                        else binding.btn12.text = "СТАРТ"
+                    }
+                }
+            }
+        }
+        binding.btn11.setOnClickListener {
+            viewModel1.pauseCounter()
+            viewModel1.resetCounter()
+        }
+
+
+
+
+
+
+        binding.btn22.setOnClickListener {
+            if (binding.btn22.text == "СТАРТ") {
+            binding.btn22.text = "СТОП"
+            viewModel2.startCounter()
+        } else {
+            binding.btn22.text = "СТАРТ"
+            viewModel2.pauseCounter()
+        }
+
+        }
+        lifecycleScope.launch {
+
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel2.getValueFlow().collect {
+                        binding.tx23.text = it.toString()
+                    }
+                }
+                launch {
+                    viewModel2.getConditionFlow().collect {
+                        if (it == true) {
+                            binding.btn22.text = "СТОП"
+                        }
+                        else binding.btn22.text = "СТАРТ"
+                    }
+                }
+            }
+        }
+
+        binding.btn21.setOnClickListener {
+            viewModel2.resetCounter()
+            viewModel2.pauseCounter()
+        }
+
+
+
+
+
+
+        binding.btn32.setOnClickListener {
+            if (binding.btn32.text == "СТАРТ") {
+                binding.btn32.text = "СТОП"
+                viewModel34.startCounter(3)
+            } else {
+                binding.btn32.text = "СТАРТ"
+                viewModel34.pauseCounter(3)
+            }
+        }
+
+        lifecycleScope.launch {
+
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel34.getValueFlow(3).collect {
+                        binding.tx33.text = it.toString()
+                    }
+                }
+                launch {
+                    viewModel34.getConditionFlow(3).collect {
+                        if (it == true) {
+                            binding.btn32.text = "СТОП"
+                        }
+                        else binding.btn32.text = "СТАРТ"
+                    }
+                }
+            }
+        }
+
+        binding.btn31.setOnClickListener {
+            viewModel34.resetCounter(3)
+            viewModel34.pauseCounter(3)
+        }
+
+
+
+
+
+        binding.btn42.setOnClickListener {
+            if (binding.btn42.text == "СТАРТ") {
+                binding.btn42.text = "СТОП"
+                viewModel34.startCounter(4)
+            } else {
+                binding.btn42.text = "СТАРТ"
+                viewModel34.pauseCounter(4)
+            }
+        }
+
+        lifecycleScope.launch {
+
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel34.getValueFlow(4).collect {
+                        binding.tx43.text = it.toString()
+                    }
+                }
+                launch {
+                    viewModel34.getConditionFlow(4).collect {
+                        if (it == true) {
+                            binding.btn42.text = "СТОП"
+                        }
+                        else binding.btn42.text = "СТАРТ"
+                    }
+                }
+            }
+        }
+
+        binding.btn41.setOnClickListener {
+            viewModel34.pauseCounter(4)
+            viewModel34.resetCounter(4)
+        }
+
+
+
+
+
+        binding.btn52.setOnClickListener {
+            if (binding.btn52.text == "СТАРТ") {
+                viewModel5.startCounter()
+            } else {
+                viewModel5.pauseCounter()
+            }
+        }
+
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                launch {
+                    viewModel5.getValueFlow().collect {
+                        binding.tx53.text = it
+                    }
+                }
+
+                launch {
+                    viewModel5.getConditionFlow().collect {
+                        if (it == true) {
+                            binding.btn52.text = "СТОП"
+                        }
+                        else binding.btn52.text = "СТАРТ"
+                    }
+                }
+            }
+        }
+
+        binding.btn51.setOnClickListener {
+            viewModel5.pauseCounter()
+            viewModel5.resetCounter()
+        }
+
+
+
+
+        binding.btnGeneralReset.setOnClickListener {
+            viewModel1.resetCounter()
+            viewModel2.resetCounter()
+            viewModel34.resetCounter(3)
+            viewModel34.resetCounter(4)
+            viewModel5.resetCounter()
+
+            viewModel1.pauseCounter()
+            viewModel2.pauseCounter()
+            viewModel34.pauseCounter(3)
+            viewModel34.pauseCounter(4)
+            viewModel5.pauseCounter()
+        }
+
+
+
+        binding.btnGeneralStart.setOnClickListener {
+            viewModel1.startCounter()
+            viewModel2.startCounter()
+            viewModel34.startCounter(5)
+            viewModel5.startCounter()
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/stopwatch/ViewModelSW.kt
@@ -0,0 +1,322 @@
+package com.example.stopwatch
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+
+class ViewModelSW1 : ViewModel() {
+
+    private val mutableValueFlow = MutableStateFlow(0)
+    val valueFlow: StateFlow<Int> = mutableValueFlow.asStateFlow()
+
+    private val mutableConditionFlow = MutableStateFlow(false)
+    val cсonditionFlow: StateFlow<Boolean> = mutableConditionFlow.asStateFlow()
+
+    var counter = 0
+
+    var job: Job? = null
+
+    fun startCounter() {
+        if (job == null) {count()}
+    }
+
+    fun pauseCounter() {
+
+        viewModelScope.launch {
+            if (job != null) {
+                job!!.cancel()
+                job = null
+            }
+            mutableConditionFlow.emit(false)
+        }
+
+    }
+
+    fun count() {
+        job = viewModelScope.launch(Dispatchers.Default) {
+            while (true) {
+                delay(1000)
+                counter++
+                mutableConditionFlow.emit(true)
+                mutableValueFlow.emit(counter)
+
+            }
+        }
+    }
+
+    fun resetCounter() {
+        counter = 0
+        viewModelScope.launch {
+            mutableValueFlow.emit(counter)
+            mutableConditionFlow.emit(false)
+        }
+    }
+
+    fun getvalueFlow(): StateFlow<Int> = valueFlow
+    fun getConditionFlow(): StateFlow<Boolean> = cсonditionFlow
+}
+
+
+
+
+class ViewModelSW2 : ViewModel() {
+    private val mutableValueFlow = MutableStateFlow(0)
+    val vvalueFlow: StateFlow<Int> = mutableValueFlow.asStateFlow()
+
+    private val mutableConditionFlow = MutableStateFlow(false)
+    val cсonditionFlow: StateFlow<Boolean> = mutableConditionFlow.asStateFlow()
+
+    var counter = 0
+
+    var job: Job? = null
+
+    val f = flow<Int> {
+        while (true) {
+            delay(1000)
+            counter++
+            emit(counter)
+        }
+    }
+
+    fun startCounter() {
+        if (job == null) {count()}
+    }
+
+    fun pauseCounter() {
+        if (job != null) {
+            job!!.cancel()
+            job = null
+        }
+        viewModelScope.launch {
+            mutableConditionFlow.emit(false)
+        }
+    }
+
+
+    fun count() {
+        job = viewModelScope.launch {
+            f.collect {
+                mutableConditionFlow.emit(true)
+                mutableValueFlow.emit(it)
+
+            }
+        }
+    }
+
+    fun resetCounter() {
+        counter = 0
+        viewModelScope.launch {
+            mutableConditionFlow.emit(false)
+            mutableValueFlow.emit(counter)
+
+        }
+    }
+
+    fun getValueFlow(): StateFlow<Int> = vvalueFlow
+    fun getConditionFlow(): StateFlow<Boolean> = cсonditionFlow
+}
+
+
+
+
+
+
+class ViewModelSW3 : ViewModel() {
+    private val mutableValueFlow3 = MutableStateFlow(0)
+    val vvvalueFlow3: StateFlow<Int> = mutableValueFlow3.asStateFlow()
+
+    private val mutableConditionFlow3 = MutableStateFlow(false)
+    val cсconditionFlow3: StateFlow<Boolean> = mutableConditionFlow3.asStateFlow()
+
+
+    private val mutableValueFlow4 = MutableStateFlow(0)
+    val vvvalueFlow4: StateFlow<Int> = mutableValueFlow4.asStateFlow()
+
+    private val mutableConditionFlow4 = MutableStateFlow(false)
+    val cсconditionFlow4: StateFlow<Boolean> = mutableConditionFlow4.asStateFlow()
+
+
+    var counterFor3Row = 0
+    var counterFor4Row = 0
+
+    var scope: CoroutineScope? = null
+
+    var job3: Job? = null
+    var job4: Job? = null
+
+    fun startCounter(numRow: Int) {
+        if (scope == null) {
+            scope = CoroutineScope(Dispatchers.Default)
+        }
+        if (numRow == 3 && job3 == null) {countForThirdRow()}
+        if (numRow == 4 && job4 == null) {countForFourthRow()}
+        if (numRow == 5 && job4 == null && job3 != null) {countForFourthRow()}
+        if (numRow == 5 && job4 != null && job3 == null) {countForThirdRow()}
+        if (numRow == 5 && job4 == null && job3 == null) {
+            countForThirdRow()
+            countForFourthRow()
+        }
+    }
+
+
+    fun pauseCounter(numRow: Int) {
+        if (numRow == 3) {
+            if (job3 != null) {
+                job3!!.cancel()
+                job3 = null
+            }
+            viewModelScope.launch {
+                mutableConditionFlow3.emit(false)
+            }
+        }
+        else {
+            if (job4 != null) {
+                job4!!.cancel()
+                job4 = null
+            }
+            viewModelScope.launch {
+                mutableConditionFlow4.emit(false)
+            }
+        }
+
+        if (job3 != null && job4 != null) {
+            if (job3!!.isCancelled && job4!!.isCancelled) {
+                scope!!.cancel()
+                scope = null
+            }
+        }
+    }
+
+
+    fun countForThirdRow() {
+
+        job3 = scope!!.launch(Dispatchers.Default) {
+            while (true) {
+                delay(1000)
+                counterFor3Row++
+                mutableConditionFlow3.emit(true)
+                mutableValueFlow3.emit(counterFor3Row)
+
+            }
+        }
+    }
+
+    fun countForFourthRow() {
+
+        job4 = scope!!.launch(Dispatchers.Default) {
+            while (true) {
+                delay(1000)
+                counterFor4Row++
+                mutableConditionFlow4.emit(true)
+                mutableValueFlow4.emit(counterFor4Row)
+
+            }
+        }
+    }
+
+    fun resetCounter(numRow: Int) {
+        if (numRow == 3) {
+            counterFor3Row = 0
+            viewModelScope.launch {
+                mutableConditionFlow3.emit(false)
+                mutableValueFlow3.emit(counterFor3Row)
+
+            }
+        }
+        else {
+            counterFor4Row = 0
+            viewModelScope.launch {
+                mutableConditionFlow4.emit(false)
+                mutableValueFlow4.emit(counterFor4Row)
+
+            }
+        }
+    }
+
+    fun getValueFlow(numRow: Int): StateFlow<Int> {
+        if (numRow == 3) {
+            return vvvalueFlow3
+        }
+        else return vvvalueFlow4
+    }
+
+    fun getConditionFlow(numRow: Int): StateFlow<Boolean> {
+        if (numRow == 3) {
+            return cсconditionFlow3
+        }
+        else return cсconditionFlow4
+    }
+}
+
+
+
+
+
+
+class ViewModelSW5 : ViewModel() {
+    private val mutableValueFlow5 = MutableStateFlow("РАЗ")
+    val vvalueFlow5: StateFlow<String> = mutableValueFlow5.asStateFlow()
+
+    private val mutableConditionFlow5 = MutableStateFlow(false)
+    val cсonditionFlow5: StateFlow<Boolean> = mutableConditionFlow5.asStateFlow()
+
+    var counter = 0
+
+    var job: Job? = null
+
+    val list = listOf<String>("РАЗ", "ДВА", "ТРИ", "ЧЕТЫРЕ")
+    
+    val f = flow<String> {
+        while (true) {
+            emit(list[counter])
+            delay(1000)
+            counter++
+            if (counter == 4) {counter = 0}
+        }
+    }
+
+    fun startCounter() {
+        if (job == null) {count()}
+    }
+
+    fun pauseCounter() {
+        if (job != null) {
+            job!!.cancel()
+            job = null
+        }
+        viewModelScope.launch {
+            mutableConditionFlow5.emit(false)
+        }
+    }
+
+
+    fun count() {
+        job = viewModelScope.launch {
+            f.collect {
+                mutableConditionFlow5.emit(true)
+                mutableValueFlow5.emit(it)
+            }
+        }
+    }
+
+    fun resetCounter() {
+        counter = 0
+        viewModelScope.launch {
+            mutableValueFlow5.emit(list[counter])
+            mutableConditionFlow5.emit(false)
+        }
+    }
+
+    fun getValueFlow(): StateFlow<String> = vvalueFlow5
+    fun getConditionFlow(): StateFlow<Boolean> = cсonditionFlow5
+}
+
